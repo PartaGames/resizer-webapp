978c3efd6862e123d733de6990f0eb6e7d4cd898
antti <antti@anttis-MacBook-Pro.local>
2017-04-12 23:00:55 +0300

    Fix serving static content

--- src/main/groovy/com/partagames/resizer/webapp/Application.groovy
+++ src/main/groovy/com/partagames/resizer/webapp/Application.groovy
@@ -3,11 +3,9 @@ package com.partagames.resizer.webapp
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @ComponentScan
 @EnableAutoConfiguration
-@EnableWebMvc
 class Application {
 
     static void main(String[] args) {
